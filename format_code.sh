9428e45cb0a3676d2ae446665ef3c6654e7b182b
elhafizh <elhafizh.hidayat@gmail.com>
2023-09-25 18:37:13 +0700

    Creating bash script for code formatting

---
+++ format_code.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+file_path="main.py"
+
+# Run black on the file
+black "$file_path"
+
+# Run isort on the file
+isort "$file_path"
+
+# Run mypy with --ignore-missing-imports on the file
+# mypy --ignore-missing-imports "$file_path"
