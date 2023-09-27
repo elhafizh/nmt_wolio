93b03b0edea7d51a1c6316a52ae42447859b65b2
elhafizh <elhafizh.hidayat@gmail.com>
2023-09-27 09:39:17 +0700

    annotated transformer: the building block of core encoder

--- lang_models/annotated_transformer/building_block.py
+++ lang_models/annotated_transformer/building_block.py
@@ -114,3 +114,18 @@ class LayerNorm(nn.Module):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class Encoder(nn.Module):
+    "Core encoder is a stack of N layers"
+
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        "Pass the input (and mask) through each layer in turn."
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
